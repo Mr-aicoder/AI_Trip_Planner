919fe2e4969803bc1ce932c3a69bdfbfbe4654b5
Sachin Tupsamundar <165402626+Mr-aicoder@users.noreply.github.com>
2026-01-22 07:43:48 +0530

    Update place_search_tool.py

--- tools/place_search_tool.py
+++ tools/place_search_tool.py
@@ -1,7 +1,7 @@
 import os
 from utils.place_info_search import GooglePlaceSearchTool TavilyPlaceSearchTool
 from typing import List
-from langchain.tools import tool 
+from langchain.tools import tool
 from dotenv import load_dotenv 
 
 
