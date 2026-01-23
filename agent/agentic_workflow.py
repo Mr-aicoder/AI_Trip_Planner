5c45fd9a628b5d998e0f44b0a92ac1dd5ac92641
Sachin Tupsamundar <165402626+Mr-aicoder@users.noreply.github.com>
2026-01-23 07:14:24 +0530

    Update agentic_workflow.py

--- agent/agentic_workflow.py
+++ agent/agentic_workflow.py
@@ -4,7 +4,7 @@ from prompt_library.prompt import  SYSTEM_PROMPT
 from langgraph.graph import StateGraph,  MessagesState, END, START 
 from langgraph.prebuilt import ToolNode,  tools_condition 
 from tools.weather_info_tool import  WeatherInfoTool 
-from tools.place_search_tool import  PlaceToolkit 
+from tools.place_search_tool import  PlaceToolkit
 from tools.expense_calculator_tool import  CalculatorToolKit
 from tools.currency_conversion_tool import CurrencyConverterToolkit
 
